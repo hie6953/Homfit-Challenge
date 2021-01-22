98af1a2bfc9606a2bfa4418337f93dbff9cf54e9
ekgml3765 <51144317+ekgml3765@users.noreply.github.com>
2021-01-22 15:52:14 +0900

    [backend] git commit test
    [backend] git commit test

--- backend/homfitchallenge/src/main/java/com/ssafy/homfit/controller/ChallengeController.java
+++ backend/homfitchallenge/src/main/java/com/ssafy/homfit/controller/ChallengeController.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ChallengeController {
 
+public static void main(String[] args) {
+	System.out.println("테스트");
+}
 }
