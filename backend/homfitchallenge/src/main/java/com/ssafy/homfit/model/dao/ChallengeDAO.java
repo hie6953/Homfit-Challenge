e39b36ba6c766426faaf2d4d7fe23f8cd387d904
ekgml3765 <51144317+ekgml3765@users.noreply.github.com>
2021-01-24 23:49:43 +0900

    [backend] 기능추가 - ChallengeDAO 구현
    [backend] 기능추가 - ChallengeDAO 구현

--- backend/homfitchallenge/src/main/java/com/ssafy/homfit/model/dao/ChallengeDAO.java
+++ backend/homfitchallenge/src/main/java/com/ssafy/homfit/model/dao/ChallengeDAO.java
@@ -1,5 +1,19 @@
 package com.ssafy.homfit.model.dao;
 
-public interface ChallengeDAO {
+import java.util.List;
 
+import com.ssafy.homfit.model.Challenge;
+
+
+public interface ChallengeDAO {
+	
+	public List<Challenge> selectAllChallenge();
+	
+	public Challenge selectChallengeByid(int challenge_id);
+	
+	public int insertChallenge(Challenge challenge);
+	
+	public int updateChallenge(Challenge challenge);
+	
+	public int deleteChallenge(int challenge_id);
 }
