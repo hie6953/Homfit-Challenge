c361071e3b5228577a57ada53f913e459bc9d661
김민중 <kimmj940920@gmail.com>
2021-01-28 01:29:51 +0900

    [backend] 기능추가 - 피드 검색 (유저 식별번호, 챌린지 식별번호, 키워드 검색

--- backend/homfitchallenge/src/main/java/com/ssafy/homfit/controller/FeedController.java
+++ backend/homfitchallenge/src/main/java/com/ssafy/homfit/controller/FeedController.java
@@ -15,7 +15,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Api("FeedController V1")
 @RestController
@@ -29,13 +30,13 @@ public class FeedController {
     private FeedService feedService;
 
     @ApiOperation(value = "피드 생성", notes = "입력된 데이터를 토대로 피드를 생성")
-    @PostMapping(value="/create")
+    @PostMapping(value = "/create")
     public ResponseEntity<String> create(@RequestBody Feed feed) {
         HttpStatus status = null;
         String msg = null;
-        
+
         try {
-            if(feedService.create(feed)){
+            if (feedService.create(feed)) {
                 msg = SUCCESS;
                 status = HttpStatus.ACCEPTED;
             } else {
@@ -50,5 +51,27 @@ public class FeedController {
 
         return new ResponseEntity<String>(msg, status);
     }
-    
+
+    @ApiOperation(value = "인증샷 수정", notes = "기존 인증샷을 지우고, 새로운 인증샷을 등록한다")
+    @PutMapping(value = "/update/img")
+    public ResponseEntity<String> putMethodName(@RequestBody Feed feed) {
+        HttpStatus status = null;
+        String msg = null;
+
+        try {
+            if (feedService.updateImg(feed)) {
+                msg = SUCCESS;
+            } else {
+                msg = FAIL;
+            }
+            status = HttpStatus.ACCEPTED;
+        } catch (Exception e) {
+            logger.error("피드 이미지 수정 실패 : {}", e);
+            msg = e.getMessage();
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return new ResponseEntity<String>(msg, status);
+    }
+
 }
--- backend/homfitchallenge/src/main/java/com/ssafy/homfit/model/service/FeedServiceImpl.java
+++ backend/homfitchallenge/src/main/java/com/ssafy/homfit/model/service/FeedServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ssafy.homfit.model.service;
 
+import java.util.List;
+
 import com.ssafy.homfit.model.Feed;
 import com.ssafy.homfit.model.dao.FeedDAO;
 
@@ -23,5 +25,53 @@ public class FeedServiceImpl implements FeedService {
         }
         return true;
     }
-    
+
+    @Override
+    public boolean updateImg(Feed feed) throws Exception {
+
+        try {
+            // 멀티파트폼 저장하는 프로세스 필요
+
+            sqlSession.getMapper(FeedDAO.class).updateImg(feed);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<Feed> searchByTag(String keyword) throws Exception {
+        List<Feed> list = null;
+        List<Integer> item = null;
+
+        item = sqlSession.getMapper(FeedDAO.class).searchCidKeyword(keyword);
+        list = sqlSession.getMapper(FeedDAO.class).searchByTag(item);
+
+        return list;
+    }
+
+    @Override
+    public Feed searchByFeedId(int feed_id) throws Exception {
+        Feed data = null;
+
+        data = sqlSession.getMapper(FeedDAO.class).searchByFeedId(feed_id);
+        return data;
+    }
+
+    @Override
+    public List<Feed> searchByUser(String uid) throws Exception {
+        List<Feed> list = null;
+        list = sqlSession.getMapper(FeedDAO.class).searchByUser(uid);
+        return list;
+    }
+
+    @Override
+    public List<Feed> searchByChallenge(int challenge_id) throws Exception {
+        List<Feed> list = null;
+        list = sqlSession.getMapper(FeedDAO.class).searchByChallenge(challenge_id);
+        return list;
+    }
+
 }
