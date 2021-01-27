9dde0bd809605ce31967221ece0117ceee2a7309
NamWoojin <nam990304@gmail.com>
2021-01-27 10:22:07 +0900

    [frontend] 기능추가 - 챌린지개설 페이지 나갈 때
    챌린지 개설 페이지 나갈 때 알림문구 출력

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
@@ -7,34 +7,38 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('../views/Login.vue')
+    component: () => import('../views/Login.vue'),
   },
   {
     path: '/forgot-password',
     name: 'forgot-password',
-    component: () => import('../views/ForgotPassword.vue')
+    component: () => import('../views/ForgotPassword.vue'),
   },
   {
     path: '/forgot-password-code',
     name: 'forgot-password-code',
-    component: () => import('../views/ForgotPasswordCode.vue')
+    component: () => import('../views/ForgotPasswordCode.vue'),
   },
   {
     path: '/forgot-password-final',
     name: 'forgot-password-final',
-    component: () => import('../views/ForgotPasswordFinal.vue')
+    component: () => import('../views/ForgotPasswordFinal.vue'),
   },
   {
     path: '/signup',
     name: 'signup',
-    component: () => import('../views/SignUp.vue')
+    component: () => import('../views/SignUp.vue'),
   },
   {
     path: '/challengelist',
     name: 'ChallengeList',
-    component: () => import('../views/ChallengeList.vue')
+    component: () => import('../views/ChallengeList.vue'),
   },
-
+  {
+    path: '/challenge-creating',
+    name: 'ChallengeCreating',
+    component: () => import('../views/ChallengeCreating.vue'),
+  },
 ];
 
 const router = new VueRouter({
@@ -42,5 +46,14 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
-
+router.beforeEach((to, from, next) => {
+  if (
+    from.name == 'ChallengeCreating' &&
+    !confirm('나가시면 챌린지 개설이 종료됩니다.\n정말 나가시겠습니까?')
+  ) {
+    next(false);
+  } else {
+    next();
+  }
+});
 export default router;
