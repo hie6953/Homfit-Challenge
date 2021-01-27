14f434de8e4810226af50421acf3edb338a292f8
NamWoojin <nam990304@gmail.com>
2021-01-27 13:23:18 +0900

    [frontend] 기능추가 - 챌린지 리스트 카드
    props로 전달받은 챌린지 정보 보여주기

--- frontend/src/components/ChallengeListCard.vue
+++ frontend/src/components/ChallengeListCard.vue
@@ -10,13 +10,13 @@
       class="mb-2 shadow"
     >
       <div class="challenge-list-card-body">
-        <span id="card-title">땅끄부부</span>
+        <span id="card-title">{{ challenge.challenge_title }}</span>
         <br />
         <img class="card-user-image" src="@/assets/NavBar/anonimous_user.png" />
-        <span id="card-user-nick-name">땅끄부부</span>
+        <span id="card-user-nick-name">{{ challenge.make_uid }}</span>
         <br />
-        <span id="card-day">월/수/금</span>
-        <span id="card-period">7일</span>
+        <span id="card-day">{{ GetDayList() }}</span>
+        <span id="card-period">{{ challenge.period }}일</span>
       </div>
       <hr class="challenge-list-card-hr" />
       <div class="challenge-list-card-footer">
@@ -27,9 +27,35 @@
 </template>
 
 <script>
+const dayList = ['', '월', '화', '수', '목', '금', '토', '일'];
+// const bodyList = [
+//   '',
+//   '전신',
+//   '상체',
+//   '하체',
+//   '가슴',
+//   '팔',
+//   '복부',
+//   '엉덩이',
+//   '다리',
+//   '기타',
+// ];
 export default {
   props: {
     challenge: Object,
+  },
+  methods: {
+    GetDayList() {
+      let list = this.challenge.daylist_string;
+      for (
+        let index = 0;
+        index < this.challnege.daylist_string.length;
+        index++
+      ) {
+        list[index] = dayList[this.challnege.daylist_string[index]];
+      }
+      return list.join('/');
+    },
   },
 };
 </script>
