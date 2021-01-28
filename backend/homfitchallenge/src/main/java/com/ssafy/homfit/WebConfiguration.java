3161c805886bf70d59b0dce3e5f6020f4c93e28b
김민중 <kimmj940920@gmail.com>
2021-01-28 11:06:07 +0900

    [backend] 기타 - 통합을 위한 저장

--- backend/homfitchallenge/src/main/java/com/ssafy/homfit/WebConfiguration.java
+++ backend/homfitchallenge/src/main/java/com/ssafy/homfit/WebConfiguration.java
@@ -1,12 +1,27 @@
 package com.ssafy.homfit;
 
+import com.ssafy.homfit.interceptor.JwtInterceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer{
-    
+    private static final String[] EXCLUED_PATHS = { "/","/user/login","/user/signup", "/error/**"};
+
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+
+    // @Override
+    // public void addInterceptors(InterceptorRegistry registry) {
+    //     registry.addInterceptor(jwtInterceptor)
+    //     .addPathPatterns("/**")
+    //     .excludePathPatterns(EXCLUED_PATHS);
+    // }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
