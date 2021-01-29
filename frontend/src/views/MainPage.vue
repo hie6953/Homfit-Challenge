f98309361c41b42c9010d45a01c1bd9f8046e244
이종희 <hie6953@naver.com>
2021-01-29 23:54:06 +0900

    [frontend] 기능추가 - 챌린지리스트
    챌린지리스트 추가

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -3,6 +3,15 @@
     <div class="container">
       <MainPageAd class="main-component-margin" />
       <MainPageCategory class="main-component-margin" />
+      <div class="main-component-margin">
+        <div class="d-flex bd-highlight px-4">
+          <div class="p-2 bd-highlight main-list">인기 챌린지</div>
+          <router-link to="/challengelist" class="ml-auto p-2 bd-highlight main-more">
+            <div class="main-more">전체보기 ></div>
+          </router-link>
+        </div>
+      </div>
+      <MainPageList class="main-content-margin" />
     </div>
   </div>
 </template>
@@ -10,12 +19,14 @@
 <script>
 import MainPageAd from "../components/MainPage/MainPageAd.vue"
 import MainPageCategory from "../components/MainPage/MainPageCategory.vue"
+import MainPageList from "../components/MainPage/MainPageList.vue"
 
 export default {
 
   components: {
     MainPageAd,
     MainPageCategory,
+    MainPageList,
   }
 }
 </script>
@@ -24,8 +35,22 @@ export default {
 .main-container {
   padding: 1rem;
 }
+
 .main-component-margin {
-  margin-bottom: 40px;
+  margin-top: 40px;
+}
+
+.main-content-margin {
+  margin-top: 20px;
+}
+
+.main-list {
+  font-weight: 700;
+  font-size: 1.5rem;
+}
+
+.main-more:hover {
+  text-decoration: none;
 }
 
 </style>
