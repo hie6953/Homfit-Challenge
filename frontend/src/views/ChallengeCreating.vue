aaebe6428694cd87ca89361f06dff7c002cd490b
NamWoojin <nam990304@gmail.com>
2021-02-01 20:09:46 +0900

    [frontend] 기능추가 - 프로그래스바
    프로그래스바 컬러 아이콘 추가
    챌린지 개설 완료 페이지 추가

--- frontend/src/views/ChallengeCreating.vue
+++ frontend/src/views/ChallengeCreating.vue
@@ -4,66 +4,62 @@
       <div class="creating-progress-bar">
         <ul id="creating-progress-list">
           <li>
-            <img src="@/assets/challengeCreating/information.png" />
+            <img
+              v-if="pageComplete_1"
+              src="@/assets/challengeCreating/information_color.png"
+            />
+            <img v-else src="@/assets/challengeCreating/information.png" />
             <br />
             <span
               class="creating-progress-check-icon"
               :class="{ 'this-page': page == 1 }"
             >
-              <b-icon v-if="pageComplete[0] == 0" icon="x"></b-icon>
-              <b-icon
-                v-if="pageComplete[0] == 1"
-                icon="arrow-clockwise"
-              ></b-icon>
-              <b-icon v-if="pageComplete[0] == 2" icon="check"></b-icon>
+              <b-icon icon="dot"></b-icon>
             </span>
             <p>소개</p>
           </li>
           <li>
-            <img src="@/assets/challengeCreating/calendar.png" />
+            <img
+              v-if="pageComplete_2"
+              src="@/assets/challengeCreating/calendar_color.png"
+            />
+            <img v-else src="@/assets/challengeCreating/calendar.png" />
             <br />
             <span
               class="creating-progress-check-icon"
               :class="{ 'this-page': page == 2 }"
             >
-              <b-icon v-if="pageComplete[1] == 0" icon="x"></b-icon>
-              <b-icon
-                v-if="pageComplete[1] == 1"
-                icon="arrow-clockwise"
-              ></b-icon>
-              <b-icon v-if="pageComplete[1] == 2" icon="check"></b-icon>
+              <b-icon icon="dot"></b-icon>
             </span>
             <p>기간</p>
           </li>
           <li>
-            <img src="@/assets/challengeCreating/certification.png" />
+            <img
+              v-if="pageComplete_3"
+              src="@/assets/challengeCreating/certification_color.png"
+            />
+            <img v-else src="@/assets/challengeCreating/certification.png" />
             <br />
             <span
               class="creating-progress-check-icon"
               :class="{ 'this-page': page == 3 }"
             >
-              <b-icon v-if="pageComplete[2] == 0" icon="x"></b-icon>
-              <b-icon
-                v-if="pageComplete[2] == 1"
-                icon="arrow-clockwise"
-              ></b-icon>
-              <b-icon v-if="pageComplete[2] == 2" icon="check"></b-icon>
+              <b-icon icon="dot"></b-icon>
             </span>
             <p>인증</p>
           </li>
           <li>
-            <img src="@/assets/challengeCreating/hashtag.png" />
+            <img
+              v-if="pageComplete_4"
+              src="@/assets/challengeCreating/hashtag_color.png"
+            />
+            <img v-else src="@/assets/challengeCreating/hashtag.png" />
             <br />
             <span
               class="creating-progress-check-icon"
               :class="{ 'this-page': page == 4 }"
             >
-              <b-icon v-if="pageComplete[3] == 0" icon="x"></b-icon>
-              <b-icon
-                v-if="pageComplete[3] == 1"
-                icon="arrow-clockwise"
-              ></b-icon>
-              <b-icon v-if="pageComplete[3] == 2" icon="check"></b-icon>
+              <b-icon icon="dot"></b-icon>
             </span>
             <p>태그</p>
           </li>
@@ -86,13 +82,13 @@
           :props_end_date="challenge.end_date"
           :props_period="challenge.period"
           :props_dayList="challenge.dayList"
+          :props_day_certify_count="challenge.day_certify_count"
           @PrevPage="PageTwoPrev"
           @NextPage="PageTwoNext"
         ></ChallengePeriod>
       </div>
       <div v-if="page == 3">
         <ChallengeCertification
-          :props_day_certify_count="challenge.day_certify_count"
           :props_challenge_certify_contents="
             challenge.challenge_certify_contents
           "
@@ -109,6 +105,16 @@
           @PrevPage="PageFourPrev"
           @CreateChallenge="CreateChallenge"
         ></ChallengeTag>
+      </div>
+      <div v-if="page == 5">
+        <h3 class="align-center">
+          "{{ challenge.challenge_title }}"<br />개설이 완료되었습니다.
+        </h3>
+        <div class="align-center mt-5">
+          <router-link to="/challengelist" id="challenge-creating-end-button"
+            >챌린지 보러가기</router-link
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -139,7 +145,10 @@ export default {
     return {
       // 페이지
       page: 1,
-      pageComplete: [1, 0, 0, 0], //0:안함, 1:진행중, 2:완료
+      pageComplete_1: false,
+      pageComplete_2: false,
+      pageComplete_3: false,
+      pageComplete_4: false,
       challenge: {
         kind: 0,
         fit_id: 1,
@@ -150,7 +159,7 @@ export default {
         start_date: null,
         end_date: null,
         dayList: [],
-        day_certify_count: 0,
+        day_certify_count: 1,
         challenge_certify_contents: '',
         good_img: '',
         bad_img: '',
@@ -184,18 +193,19 @@ export default {
         tempDayList[index] = parseInt(this.challenge.dayList[index]);
       }
       this.challenge.dayList = tempDayList.sort();
-
+      this.pageComplete_4 = true;
       // 챌린지 개설 axios
       console.log(this.challenge);
       axios
         .post(`${SERVER_URL}/challenge`, this.challenge)
         .then(() => {
           alert('챌린지가 생성되었습니다.');
-          this.$router.push('/challengelist');
         })
         .catch(() => {
           alert('등록 처리시 에러가 발생했습니다.');
         });
+
+      this.page = 5;
     },
 
     // 1페이지
@@ -211,45 +221,52 @@ export default {
       this.challenge.bodyList = bodyList;
       this.challenge.challenge_title = challenge_title;
       this.challenge.challenge_contents = challenge_contents;
+      this.pageComplete_1 = true;
       this.NextPage();
     },
 
     // 2페이지
-    PageTwoPrev: function(start_date, end_date) {
+    PageTwoPrev: function(
+      start_date,
+      end_date,
+      period,
+      dayList,
+      day_certify_count
+    ) {
       this.challenge.start_date = start_date;
       this.challenge.end_date = end_date;
+      this.challenge.period = period;
+      this.challenge.dayList = dayList.sort();
+      this.challenge.day_certify_count = day_certify_count;
 
       this.PrevPage();
     },
-    PageTwoNext: function(start_date, end_date) {
+    PageTwoNext: function(
+      start_date,
+      end_date,
+      period,
+      dayList,
+      day_certify_count
+    ) {
       this.challenge.start_date = start_date;
       this.challenge.end_date = end_date;
+      this.challenge.period = period;
+      this.challenge.dayList = dayList.sort();
+      this.challenge.day_certify_count = day_certify_count;
+      this.pageComplete_2 = true;
       this.NextPage();
     },
 
     // 3페이지
-    PageThreePrev: function(
-      dayList,
-      day_certify_count,
-      challenge_certify_contents,
-      only_cam
-    ) {
-      this.challenge.dayList = dayList;
-      this.challenge.day_certify_count = day_certify_count;
+    PageThreePrev: function(challenge_certify_contents, only_cam) {
       this.challenge.challenge_certify_contents = challenge_certify_contents;
       this.challenge.only_cam = only_cam;
       this.PrevPage();
     },
-    PageThreeNext: function(
-      dayList,
-      day_certify_count,
-      challenge_certify_contents,
-      only_cam
-    ) {
-      this.challenge.dayList = dayList;
-      this.challenge.day_certify_count = day_certify_count;
+    PageThreeNext: function(challenge_certify_contents, only_cam) {
       this.challenge.challenge_certify_contents = challenge_certify_contents;
       this.challenge.only_cam = only_cam;
+      this.pageComplete_3 = true;
       this.NextPage();
     },
 
@@ -261,9 +278,7 @@ export default {
 
     // 페이지전환
     NextPage: function() {
-      this.pageComplete[this.page] == 2;
       ++this.page;
-      this.pageComplete[this.page] == 1;
     },
     PrevPage: function() {
       --this.page;
