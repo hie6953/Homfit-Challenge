03ecfd2aede9b22210679bfb94583e19a5cb9cd6
eunsu27 <pesu1027@gmail.com>
2021-02-01 11:49:43 +0900

    [frontend] 기능수정 - 프로필수정
    프로필수정페이지 수정

--- frontend/src/views/ProfileEdit.vue
+++ frontend/src/views/ProfileEdit.vue
@@ -4,21 +4,24 @@
 
     <div class="mx-auto col-8 profile-edit-container">
       <div class="row">
-        <!-- left column -->
-        <div class="col-md-3">
-          <div class="text-center">
+        <!-- 이미지 수정 -->
+        <div class="col-md-4">
+          <div class="edit-user-photo">
             <img src="@/assets/NavBar/anonimous_user.png" class="user-img" />
+            <label class="input-file-button" for="input-file">
+              사진선택
+            </label>
 
-            <input type="file" class="form-control" />
+            <input type="file" id="input-file" class="form-control-photo" />
           </div>
         </div>
 
-        <!-- edit form column -->
-        <div class="col-md-9 personal-info">
-          <form class="form-horizontal" role="form">
+        <!-- 사용자 정보 수정 -->
+        <div class="col-md-8 personal-info">
+          <form class="form-horizontal">
             <div class="form-group">
               <label class="col-lg-3 control-label">이메일</label>
-              <div class="col-lg-8">
+              <div class="col-lg-8 control-input">
                 <input
                   class="form-control"
                   type="text"
@@ -29,25 +32,25 @@
             </div>
             <div class="form-group">
               <label class="col-lg-3 control-label">닉네임</label>
-              <div class="col-lg-8">
+              <div class="col-lg-8 control-input">
                 <input class="form-control" type="text" />
               </div>
             </div>
             <div class="form-group">
               <label class="col-lg-3 control-label">휴대폰번호</label>
-              <div class="col-lg-8">
+              <div class="col-lg-8 control-input">
                 <input class="form-control" type="text" />
               </div>
             </div>
             <div class="form-group">
               <label class="col-lg-3 control-label">성별</label>
-              <div class="col-lg-8">
+              <div class="col-lg-8 control-input">
                 <input class="form-control" type="text" />
               </div>
             </div>
             <div class="form-group">
               <label class="col-lg-3 control-label">연령</label>
-              <div class="col-lg-8">
+              <div class="col-lg-8 control-input">
                 <div class="ui-select">
                   <select id="user_time_zone" class="form-control">
                     <option value="Hawaii">(GMT-10:00) Hawaii</option>
