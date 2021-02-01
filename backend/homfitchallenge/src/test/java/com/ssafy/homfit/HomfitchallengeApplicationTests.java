2eed1230d46ef5c2db6e1aa377b8f7d1a1b6f308
김민중 <kimmj940920@gmail.com>
2021-02-01 11:50:34 +0900

    [backend] 기능추가 - 레디스 연동

--- backend/homfitchallenge/src/test/java/com/ssafy/homfit/HomfitchallengeApplicationTests.java
+++ backend/homfitchallenge/src/test/java/com/ssafy/homfit/HomfitchallengeApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ssafy.homfit;
 
+import com.ssafy.homfit.api.ConnectToRedis;
 import com.ssafy.homfit.util.Util;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,12 @@ class HomfitchallengeApplicationTests {
 	void contextLoads() {
 		String tk = Util.generateToken();
 		System.out.println(tk);
+	}
+
+	@Test
+	void redisTest(){
+		ConnectToRedis s = new ConnectToRedis();
+		s.setCache("hi", "hello");
 	}
 
 }
