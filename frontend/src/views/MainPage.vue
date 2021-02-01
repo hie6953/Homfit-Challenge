7ae95bd37e68a362ef4de3152a93a1c04e0fe11a
이종희 <hie6953@naver.com>
2021-02-01 11:53:53 +0900

    [frontend] 기능수정 - 메인 챌린지 리스트 캐러셀
    전체보기 라우터링크 수정, 챌린지 리스트에 캐러셀 및 임시 데이터 추가

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -6,12 +6,24 @@
       <div class="main-component-margin">
         <div class="d-flex bd-highlight px-4">
           <div class="p-2 bd-highlight main-list">인기 챌린지</div>
-          <router-link to="/challengelist" class="ml-auto p-2 bd-highlight main-more">
+          <router-link :to="{
+            name: 'ChallengeListCategory',
+            params: { category_number: 3, category_sort: 1 },
+          }" class="ml-auto p-2 bd-highlight main-more">
             <div class="main-more">전체보기 ></div>
           </router-link>
         </div>
       </div>
-      <MainPageList class="main-content-margin" />
+      <MainPageList
+        v-if="isWeb === true"
+        class="main-content-margin"
+        :mainPageList="popularChallengeList"
+      />
+      <MainPageList2
+        v-if="isWeb === false"
+        class="main-content-margin"
+        :mainPageList="popularChallengeList2"
+      />
       <div class="main-component-margin">
         <div class="d-flex bd-highlight px-4">
           <div class="p-2 bd-highlight main-list">신규 챌린지</div>
@@ -20,7 +32,10 @@
           </router-link>
         </div>
       </div>
-      <MainPageList class="main-content-margin" />
+      <MainPageList
+        class="main-content-margin"
+        :mainPageList="newChallengeList"
+      />
     </div>
   </div>
 </template>
@@ -29,13 +44,64 @@
 import MainPageAd from "../components/MainPage/MainPageAd.vue"
 import MainPageCategory from "../components/MainPage/MainPageCategory.vue"
 import MainPageList from "../components/MainPage/MainPageList.vue"
+import MainPageList2 from "../components/MainPage/MainPageList2.vue"
+
+// import axios from 'axios';
+// const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 
 export default {
-
+// 신규챌린지와 인기챌린지 10개씩 받아오기
+// 받아온 리스트 MainPageList로 데이터 보내기
   components: {
     MainPageAd,
     MainPageCategory,
     MainPageList,
+    MainPageList2,
+  },
+  data() {
+    return {
+      isWeb: false,
+      newChallengeList: [
+        // 예시로 넣어둠
+        {name: 'Kin Khao', tag: ["Thai"]},
+        {name: 'Jū-Ni', tag: ["Sushi", "Japanese", "$$$$"]},
+        {name: 'Delfina', tag: ["Pizza", "Casual"]},
+        {name: 'San Tung', tag: ["Chinese", "$$"]},
+        {name: 'Anchor Oyster Bar', tag: ["Seafood", "Cioppino"]},
+        {name: 'Locanda', tag: ["Italian"]},
+        {name: 'Garden Creamery', tag: ["Ice cream"]},
+      ],
+      popularChallengeList: [
+        // 예시로 넣어둠
+        {name: 'Kin Khao', tag: ["Thai"]},
+        {name: 'Jū-Ni', tag: ["Sushi", "Japanese", "$$$$"]},
+        {name: 'Delfina', tag: ["Pizza", "Casual"]},
+        {name: 'San Tung', tag: ["Chinese", "$$"]},
+        {name: 'Anchor Oyster Bar', tag: ["Seafood", "Cioppino"]},
+        {name: 'Locanda', tag: ["Italian"]},
+        {name: 'Garden Creamery', tag: ["Ice cream"]},
+      ],
+      popularChallengeList2: [
+        // 예시로 넣어둠
+        {name: '2번', tag: ["Thai"]},
+        {name: 'Jū-Ni', tag: ["Sushi", "Japanese", "$$$$"]},
+        {name: 'Delfina', tag: ["Pizza", "Casual"]},
+        {name: 'San Tung', tag: ["Chinese", "$$"]},
+        {name: 'Anchor Oyster Bar', tag: ["Seafood", "Cioppino"]},
+        {name: 'Locanda', tag: ["Italian"]},
+        {name: 'Garden Creamery', tag: ["Ice cream"]},
+      ],
+    }
+  },
+  created() {
+    // axios
+      // .get(`${SERVER_URL}/challenge/all`)
+      // .then(({ data }) => {
+      //   this.challengeList = data;
+      // })
+      // .catch(() => {
+      //   alert('챌린지 목록을 불러오지 못했습니다.');
+      // });
   }
 }
 </script>
