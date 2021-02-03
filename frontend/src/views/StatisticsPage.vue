9c5978ed94a9ae6b86c96d12f6ccee0b94329ce5
이종희 <hie6953@naver.com>
2021-02-03 23:55:20 +0900

    [frontend] 기능추가 - 월/년 슬라이드 버튼

---
+++ frontend/src/views/StatisticsPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <StatisticsTermTag />
+  </div>
+</template>
+
+<script>
+import StatisticsTermTag from "../components/StatisticsPage/StatisticsTermTag"
+
+export default {
+  components: {
+    StatisticsTermTag,
+  }
+}
+</script>
+
+<style>
+
+</style>
