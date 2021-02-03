ce3d157e03f33b9aee55157ce858b263f22dd32b
NamWoojin <nam990304@gmail.com>
2021-02-03 23:03:20 +0900

    [frontend] 기능추가 - 챌린지 상세 페이지 레이아웃
    챌린지 상세 페이지 레이아웃 구성

---
+++ frontend/src/components/ChallengeMoreInfo/ChallengeTitle.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="challenge-title-background">
+        <div class="row col-11 col-md-8 mx-auto">
+        <div class="col-12 col-md-6">
+
+        <img id="challenge-img" src="https://picsum.photos/600/300/?image=25">
+        </div>
+        <div class="col-12 col-md-6 my-auto">
+            <div class="challenge-info">
+                <span id="title-check-date">시작전</span>
+                <span>카테고리(글씨 더 작게)</span>
+                <h2>땅끄부부</h2>
+                <span>아이콘+개설자</span>
+                <br>
+                <span>요일+하루인증횟수+기간+날짜</span>
+                <br>
+                <span>참여자수</span>
+                <br>
+                <span>태그</span>
+            </div>
+        </div>
+
+        </div>
+    </div>
+</template>
+<script>
+import '@/assets/css/ChallengeMoreInfo/challengeTitle.css';
+
+// const check_date = ["시작전","진행중","완료"];
+export default {
+    components:{
+    },
+
+}
+</script>
---
+++ frontend/src/views/ChallengeMoreInfo.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <challenge-title></challenge-title>
+    <div id="info-navbar">
+      <b-button>설명</b-button>
+      <b-button>인증방법</b-button>
+      <b-button>역대결과</b-button>
+      <b-button>후기</b-button>
+    </div>
+      <div class="row col-11 col-lg-8 mx-auto">
+        <div class="col-12 col-lg-8">
+          <br />
+          <img src="@/assets/logo.png" alt="" />
+          <br />
+          <div style="background-color:red;">너비</div>
+          <img src="@/assets/logo.png" alt="" /><br />
+          <img src="@/assets/logo.png" alt="" /><br />
+          <img src="@/assets/logo.png" alt="" /><br />
+          <img src="@/assets/logo.png" alt="" /><br />
+          <img src="@/assets/logo.png" alt="" />
+        </div>
+
+        <div v-if="!isMobile" class="col-12 col-lg-4">
+          <div class="info-float">
+              <div >
+
+              <div style="background-color:blue;">신청버튼</div>
+              </div>
+            
+          </div>
+        </div>
+      </div>
+    <div v-if="isMobile">
+      <div class="info-float">
+          <div style="background-color:blue;">신청버튼</div>
+        
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import ChallengeTitle from "@/components/ChallengeMoreInfo/ChallengeTitle.vue";
+import "@/assets/css/ChallengeMoreInfo/challengeMoreInfo.css";
+
+export default {
+  components: {
+    ChallengeTitle,
+  },
+  data() {
+    return {
+      isMobile: false,
+    };
+  },
+  methods: {
+    // 화면 너비에 따른 모바일 여부 판단
+    handleResize: function() {
+      this.isMobile = window.innerWidth <= 992;
+    },
+  },
+  mounted() {
+    // 화면 너비 측정 이벤트 추가/
+    this.handleResize();
+    window.addEventListener("resize", this.handleResize);
+  },
+};
+</script>
