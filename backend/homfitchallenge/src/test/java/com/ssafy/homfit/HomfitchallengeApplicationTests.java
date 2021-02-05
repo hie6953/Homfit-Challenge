7d52b529c0d27ea5d942a6c6d429ec0dac92106d
김민중 <kimmj940920@gmail.com>
2021-02-05 23:31:32 +0900

    [backend] 버그 -버그fix 및 회원 가입 테스트 코드 작성

--- backend/homfitchallenge/src/test/java/com/ssafy/homfit/HomfitchallengeApplicationTests.java
+++ backend/homfitchallenge/src/test/java/com/ssafy/homfit/HomfitchallengeApplicationTests.java
@@ -1,6 +1,8 @@
 package com.ssafy.homfit;
 
 
+import com.ssafy.homfit.model.User;
+import com.ssafy.homfit.model.service.UserServiceImpl;
 import com.ssafy.homfit.util.Util;
 
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,27 @@ class HomfitchallengeApplicationTests {
 	void contextLoads() {
 		String tk = Util.generateToken();
 		System.out.println(tk);
+	}
+
+	@Test
+	void signUpTest(){
+		UserServiceImpl userServiceImpl = new UserServiceImpl();
+		User user = new User();
+		user.setUid(Util.generateToken());
+		String uidToken = null;
+
+		try {
+			while (true) {
+				uidToken = Util.generateToken();
+				if (userServiceImpl.getUid(uidToken) == null)
+					break;
+			}
+			user.setUid(uidToken);
+			userServiceImpl.signup(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
