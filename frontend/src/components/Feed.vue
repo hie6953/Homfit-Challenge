25c16be187670fa061ca15f7c58f51a8334347ba
eunsu27 <pesu1027@gmail.com>
2021-02-08 22:33:06 +0900

    [frontend] 기능수정 - 검색페이지
    feed.vue 모바일버전 수정
    검색바 드롭다운 추가
    css 수정
    챌린지카드 사용

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,29 +1,46 @@
 <template>
   <div class="mt-3">
-    <hr id="hr-top" />
-
     <div class="feed-container">
-      <div class="grid">
-        <!-- 사진1 -->
-        <div class="gallery-item">
+      <div class="row grid">
+        <!-- axios -->
+        <!-- <div class="col-xs-4 gallery-item">
           <img
             src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?w=500&h=500&fit=crop"
             class="responsive-image"
-            alt=""
+            @click="ChallengeMoreInfo"
           />
           <div class="gallery-item-info">
             <ul>
               <li class="gallery-item-likes">
-                <span class="visually-hidden">운동하쟙</span>
-                <!-- <i class="fas fa-heart" aria-hidden="true"></i> 56
-              </li> -->
+                <span class="visually-hidden">{{
+                  challenge.challenge_title
+                }}</span>
+              </li>
+            </ul>
+          </div>
+        </div> -->
+
+        <!-- 사진1 -->
+        <div class="col-xs-4 gallery-item">
+          <img
+            src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?w=500&h=500&fit=crop"
+            class="responsive-image"
+            @click="ChallengeMoreInfo"
+          />
+          <div class="gallery-item-info">
+            <ul>
+              <li class="gallery-item-likes">
+                <span class="visually-hidden"
+                  >안녕 우리 함께 운동하쟙안녕 우리 함께 운동하쟙안녕 우리 함께
+                  운동하쟙</span
+                >
               </li>
             </ul>
           </div>
         </div>
 
         <!-- 사진2 -->
-        <div class="gallery-item">
+        <div class="col-xs-4 gallery-item">
           <img
             src="https://images.unsplash.com/photo-1502630859934-b3b41d18206c?w=500&h=500&fit=crop"
             class="responsive-image"
@@ -31,7 +48,7 @@
         </div>
 
         <!-- 사진3 -->
-        <div class="gallery-item">
+        <div class="col-xs-4 gallery-item">
           <img
             src="https://images.unsplash.com/photo-1498471731312-b6d2b8280c61?w=500&h=500&fit=crop"
             class="responsive-image"
@@ -39,7 +56,7 @@
         </div>
 
         <!-- 사진4 -->
-        <div class="gallery-item">
+        <div class="col-xs-4 gallery-item">
           <img
             src="https://images.unsplash.com/photo-1515023115689-589c33041d3c?w=500&h=500&fit=crop"
             class="responsive-image"
@@ -55,8 +72,16 @@ import '../assets/css/feed.css';
 
 export default {
   name: 'Feed',
+  props: {
+    challenge: Object,
+  },
   data: function() {
     return {};
+  },
+  methods: {
+    ChallengeMoreInfo: function() {
+      this.$emit('moreInfo', this.challenge.challenge_id);
+    },
   },
 };
 </script>
