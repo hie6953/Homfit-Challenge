b2d57b1b45872bf8c45cab6a3da07076d26dfdef
eunsu27 <pesu1027@gmail.com>
2021-02-09 14:40:17 +0900

    [frontend] 기능수정 - 비밀번호찾기
    비밀번호 찾기 axios 연결
    페이지 레이아웃 수정(이메일 입력, 인증번호 입력 페이지 통합)
    css 수정

--- frontend/src/views/ForgotPasswordFinal.vue
+++ frontend/src/views/ForgotPasswordFinal.vue
@@ -5,8 +5,8 @@
         <h3>비밀번호 변경</h3>
         <input
           id="email"
-          placeholder="ssafy@ssafy.com"
           class="sign-in-form__form__input form-control sign-in-form__form__email"
+          v-model="email"
           readonly
         />
 
@@ -16,6 +16,7 @@
           class="sign-in-form__form__input form-control"
           type="password"
           id="password"
+          v-model="password"
         />
 
         <input
@@ -24,12 +25,12 @@
           class="sign-in-form__form__input form-control"
           type="password"
           id="password-confirm"
+          v-model="passwordcheck"
         />
 
         <button
           class="sign-in-form__form__submit btn btn-priority"
-          @click="onPassword"
-          :disabled="!isSubmit"
+          @click="UserPasswordEdit()"
         >
           저장
         </button>
@@ -39,9 +40,75 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import '../assets/css/forgotpassword.css';
+import axios from 'axios';
+const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 
 export default {
-  data: () => ({}),
+  name: 'ForgotPassword',
+  data: function() {
+    return {
+      password: '',
+      passwordcheck: '',
+      email: '',
+    };
+  },
+  watch: {
+    password(value) {
+      this.password = value;
+      this.checkPassword(value);
+    },
+    passwordcheck(value) {
+      this.passwordcheck = value;
+      this.checkPasswordconfirm(value);
+    },
+  },
+  computed: {
+    ...mapGetters(['getUserEmail']),
+  },
+  mounted() {
+    this.setEmail();
+  },
+  methods: {
+    UserPasswordEdit() {
+      if (
+        this.password == this.passwordcheck &&
+        this.password.length > 0 &&
+        this.passwordcheck.length > 0
+      ) {
+        axios
+          .post(`${SERVER_URL}/user/findPw`, {
+            password: this.password,
+            email: this.email,
+          })
+
+          .then(({ data }) => {
+            console.log(data);
+            if (data == 'success') {
+              alert('비밀번호가 변경되었습니다.');
+              this.$router.replace('/login');
+            }
+          })
+          .catch(() => {
+            alert('에러가 발생했습니다.');
+          });
+      }
+    },
+
+    setEmail() {
+      this.email = this.getUserEmail;
+    },
+    checkPassword(value) {
+      if (value.length < 8) {
+        this.errormsg['password'] = `비밀번호를 8자 이상으로 입력해주세요.`;
+      } else this.errormsg['password'] = ``;
+    },
+    checkPasswordconfirm(value) {
+      if (value.length >= 0 && this.password != this.passwordcheck) {
+        this.errormsg['passwordcheck'] = `비밀번호가 일치하지 않습니다.`;
+      } else this.errormsg['passwordcheck'] = ``;
+    },
+  },
 };
 </script>
