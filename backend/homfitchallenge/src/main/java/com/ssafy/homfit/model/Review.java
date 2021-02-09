a47f8aff353eab196b24eca2d4acafee9d1cee37
ekgml3765 <51144317+ekgml3765@users.noreply.github.com>
2021-02-09 16:53:44 +0900

    [backend] 기능추가 - Review
    [backend] 기능추가 - Review

---
+++ backend/homfitchallenge/src/main/java/com/ssafy/homfit/model/Review.java
@@ -0,0 +1,13 @@
+package com.ssafy.homfit.model;
+
+import java.sql.Timestamp;
+
+public class Review {
+
+	private String review_id;
+	private String before_img;
+	private String after_img;
+	private int challenge_id;
+	private String uid;
+	//private Timestamp //now();
+}
