ea2ba32b58f14c8e233add101daeae1902fdda47
NamWoojin <nam990304@gmail.com>
2021-02-11 10:49:16 +0900

    [frontend] 기능수정 - navbar모바일
    hover효과 제거

--- frontend/src/components/NavBars/NavBarSecondMobile.vue
+++ frontend/src/components/NavBars/NavBarSecondMobile.vue
@@ -4,35 +4,35 @@
     <b-navbar class="navbar-second-menu px-0" variant="white">
       <!-- 메뉴 -->
       <b-navbar-nav class="navbar-second-menu-inside">
-        <router-link to="/challengelist" class="second-menu mobile">
+        <router-link to="/challengelist" class="second-menu-mobile">
           <div class="navbar-second-icon">
             <img src="@/assets/NavBar/list.png" />
             <br />
             <span>챌린지 리스트</span>
           </div>
         </router-link>
-        <router-link to="/feed" class="second-menu mobile">
+        <router-link to="/feed" class="second-menu-mobile">
           <div class="navbar-second-icon">
             <img src="@/assets/NavBar/feed.png" />
             <br />
             <span>피드</span>
           </div>
         </router-link>
-        <router-link to="/challenge-creating" class="second-menu mobile">
+        <router-link to="/challenge-creating" class="second-menu-mobile">
           <div class="navbar-second-icon">
             <img src="@/assets/NavBar/create.png" />
             <br />
             <span>챌린지 개설</span>
           </div>
         </router-link>
-        <router-link to="/" class="second-menu mobile">
+        <router-link to="/" class="second-menu-mobile">
           <div class="navbar-second-icon">
             <img src="@/assets/NavBar/recommend.png" />
             <br />
             <span>추천 챌린지</span>
           </div>
         </router-link>
-        <router-link to="/" class="second-menu mobile">
+        <router-link to="/" class="second-menu-mobile">
           <div class="navbar-second-icon">
             <img src="@/assets/NavBar/my_challenge.png" />
             <br />
