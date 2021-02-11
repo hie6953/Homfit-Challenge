851d0913d2aa01d843e70338e51613138a047574
이종희 <hie6953@naver.com>
2021-02-11 10:17:37 +0900

    [frontend]기능수정 - xl사이즈 제거, 콘솔출력제거
    col-xl 제거, 콘솔 출력 제거

--- frontend/src/views/Bookmark.vue
+++ frontend/src/views/Bookmark.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-12 col-md-8 col-xl-6 bookmark-container">
+  <div class="col-12 col-md-8 bookmark-container">
     <p class="bookmark-title">챌린지 북마크</p>
     <div class="row list-card">
       <ChallengeListCard
@@ -45,11 +45,9 @@ export default {
     ...mapGetters(["getUserUid"]),
   },
   created() {
-    console.log(this.getUserUid)
     axios
       .get(`${SERVER_URL}/challenge/bookmark/${this.getUserUid}`)
       .then((data) => {
-        console.log(data.data)
         this.challengeList = data.data
       })
       .catch(() => {
