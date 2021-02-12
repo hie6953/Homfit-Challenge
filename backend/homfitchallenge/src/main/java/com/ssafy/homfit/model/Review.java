e7568f369ff6487553b5948e7e28321c96d92bad
ekgml3765 <51144317+ekgml3765@users.noreply.github.com>
2021-02-12 19:03:21 +0900

    [backend] 기능수정 - 후기 전후사진 삭제
    [backend] 기능수정 - 후기 전후사진 삭제

--- backend/homfitchallenge/src/main/java/com/ssafy/homfit/model/Review.java
+++ backend/homfitchallenge/src/main/java/com/ssafy/homfit/model/Review.java
@@ -4,10 +4,52 @@ import java.sql.Timestamp;
 
 public class Review {
 
-	private String review_id;
-	private String before_img;
-	private String after_img;
+
+
 	private int challenge_id;
 	private String uid;
-	//private Timestamp //now();
+	private int star_point;
+	private String review_contents;
+	private Timestamp regist_time;
+	
+
+	public int getChallenge_id() {
+		return challenge_id;
+	}
+	public void setChallenge_id(int challenge_id) {
+		this.challenge_id = challenge_id;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public int getStar_point() {
+		return star_point;
+	}
+	public void setStar_point(int star_point) {
+		this.star_point = star_point;
+	}
+	public String getReview_contents() {
+		return review_contents;
+	}
+	public void setReview_contents(String review_contents) {
+		this.review_contents = review_contents;
+	}
+	public Timestamp getRegist_time() {
+		return regist_time;
+	}
+	public void setRegist_time(Timestamp regist_time) {
+		this.regist_time = regist_time;
+	}
+	@Override
+	public String toString() {
+		return "Review [challenge_id=" + challenge_id + ", uid=" + uid + ", star_point=" + star_point
+				+ ", review_contents=" + review_contents + ", regist_time=" + regist_time + "]";
+	}
+	
+	
+	
+	
 }
