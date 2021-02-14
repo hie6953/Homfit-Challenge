662f408a14706c2dccbf344b55ac53dd99691542
ekgml3765 <51144317+ekgml3765@users.noreply.github.com>
2021-02-14 21:57:19 +0900

    [backend] 기능추가 - 챌린지 참여상세 챌린지 담기
    [backend] 기능추가 - 챌린지 참여상세 챌린지 담기

--- backend/homfitchallenge/src/main/java/com/ssafy/homfit/controller/ChallengeController.java
+++ backend/homfitchallenge/src/main/java/com/ssafy/homfit/controller/ChallengeController.java
@@ -171,6 +171,7 @@ public class ChallengeController {
 		} else {
 			Challenge challenge = opt.get();
 
+			map.put("challenge", challenge);
 			int average_rate = challenge.getAverage_rate();
 			double total_cnt = challenge.getCertification(); // 소수점계산을 위해
 			int user_cnt = feedService.selectUserFeed(uid, challengeId).length;
