7b2c80eea5553543de17ac8ed34c92caf0350074
김민중 <kimmj940920@gmail.com>
2021-02-15 17:40:51 +0900

    [backend] 기타 - 트랜잭션 설정

--- backend/homfitchallenge/src/main/java/com/ssafy/homfit/controller/FeedController.java
+++ backend/homfitchallenge/src/main/java/com/ssafy/homfit/controller/FeedController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -91,6 +92,7 @@ public class FeedController {
 
     @ApiOperation(value = "좋아요 수정", notes = "회원이 피드를 좋아할 경우 좋아요 삭제, 아닐경우 좋아요 생성")
     @PutMapping(value = "/like")
+    @Transactional
     public ResponseEntity<Integer> updateLikes(@RequestBody LikeVO like) {
         int count = -1;
         HttpStatus status = null;
