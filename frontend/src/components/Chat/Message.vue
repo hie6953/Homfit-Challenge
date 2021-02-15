e3432e02afb024e4a04a245bc4c7ee4ef2c9b13e
이종희 <hie6953@naver.com>
2021-02-15 11:18:28 +0900

    [frontend]기능수정 - 채팅입력시간, 입력창,인증/후기버튼
    실시간 채팅입력시간 표시, 버튼 비활성화조건 재설정, 채팅입력창 위치조정

--- frontend/src/components/Chat/Message.vue
+++ frontend/src/components/Chat/Message.vue
@@ -1,33 +1,28 @@
 <template>
   <div>
     <div v-if="msg.uid == getUserUid" class="me px-3">
-      <!-- <div class="chat-nickname">
-        {{msg.nick_name}}
-        <img src="@/assets/NavBar/anonimous_user.png" alt="user_img" class="user-img">
-      </div> -->
       <div class="row justify-content-end">
-        <div class="my-chat-register-date col-4 col-sm-4 mt-auto mr-2 pr-0">
-          {{msg.register_date}}
-          <!-- 오후 4:20 -->
+        <div class="my-chat-register-date col-4 col-sm-4 mt-auto mr-2 px-0">
+          {{ this.date }} {{ this.time }}
         </div>
         <div class="my-chat-msg col-7 col-sm-4">
           {{msg.content}}
         </div>
       </div>
     </div>
-    <div v-else class="other px-3">
+    
+    <div v-else class="other">
       <div class="chat-nickname">
-        <img src="@/assets/NavBar/anonimous_user.png" alt="user_img" class="user-img">
-        <!-- <img src="msg.user_img" alt="user_img" class="user-img"> -->
+        <img v-if="msg.user_img == null" src="@/assets/NavBar/anonimous_user.png" alt="user_img" class="user-img">
+        <img v-else :src="msg.user_img" alt="user_img" class="user-img">
         {{msg.nick_name}}
       </div>
-      <div class="row">
+      <div class="row px-3">
         <div class="other-chat-msg col-7 col-sm-4">
           {{msg.content}}
         </div>
-        <div class="other-chat-register-date col-4 col-sm-4 mt-auto ml-2 pl-0">
-          <!-- {{msg.register_date}} -->
-          오후 4:19
+        <div class="other-chat-register-date col-4 col-sm-4 mt-auto ml-2 px-0">
+          {{ this.date }} {{ this.time }}
         </div>
       </div>
     </div>
@@ -42,8 +37,34 @@ export default {
   props: {
     msg: Object,
   },
+  data() {
+    return {
+      date: "",
+      time: "",
+    }
+  },
   computed: {
     ...mapGetters(["getUserUid"]),
+  },
+  mounted() {
+    this.updateTime()
+  },
+  methods: {
+    updateTime() {
+      if (this.msg.regist_date) {
+        this.date = this.msg.regist_date.slice(5,10)
+        this.time = this.msg.regist_date.slice(11,16)
+      } else {
+        const now = new Date();
+        this.now *= 1000
+        const nowMonth = now.getMonth()+1 > 9 ? (now.getMonth()+1).toString() : "0"+(now.getMonth()+1).toString()
+        const nowDate = now.getDate() > 9 ? now.getDate().toString() : "0"+now.getDate().toString()
+        const nowHours = now.getHours() > 9 ? now.getHours().toString() : "0"+now.getHours().toString()
+        const nowMinutes = now.getMinutes() > 9 ? now.getMinutes().toString() : "0"+now.getMinutes().toString()
+        this.date = nowMonth+"-"+nowDate
+        this.time = nowHours+":"+nowMinutes
+      }
+    }
   },
 };
 </script>
