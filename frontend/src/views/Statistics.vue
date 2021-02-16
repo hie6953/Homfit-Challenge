8bb65e56e2c53742c42915355827cc52be5aa293
NamWoojin <nam990304@gmail.com>
2021-02-17 01:39:54 +0900

    [frontend] 기능추가 - 통계페이지 axios연결
    통계페이지 월별통계, 참여통계 axios연결

--- frontend/src/views/Statistics.vue
+++ frontend/src/views/Statistics.vue
@@ -1,56 +1,67 @@
 <template>
-  <div class="col-12 col-md-8 mx-auto">
-    <b-tabs content-class="mt-3">
-      <b-tab :class="{active:tab == 1}" title="월별 통계" @click="UpdateData()" active>
+  <div>
+    <b-tabs
+      lazy
+      v-model="tab"
+      content-class="mt-3 col-12 col-md-8 mx-auto"
+      class="statistics-menu-bar"
+    >
+      <b-tab
+        :class="{ active: tab == 0 }"
+        title="월별 통계"
+        @click="UpdateData(1)"
+        active
+      >
         <div class="statistics-background">
-          <Calendar 
-            @selectedMonth="UpdateMonth"
-          />
+          <Calendar @selectedMonth="UpdateMonth" />
         </div>
         <p class="statistics-tip">
-          <b-icon icon="info-circle" scale=1></b-icon>
-          통계는 챌린지 완료일 기준이며, 인증률이 0%일 경우 데이터를 제공하지 않습니다.</p>
+          <b-icon icon="info-circle" scale="1"></b-icon>
+          통계는 챌린지 완료일 기준이며, 인증률이 0%일 경우 데이터를 제공하지
+          않습니다.
+        </p>
         <p class="statistics-title">월별 챌린지 인증률</p>
-        <MonthGraph
-          :monthList="statisticsData.monthList"
-        />
+        <MonthGraph :monthList="statisticsData1.monthList" />
         <AverageRate
-          :current="statisticsData.current_average_rate"
-          :previous="statisticsData.previous_average_rate"
+          :current="statisticsData1.current_average_rate"
+          :previous="statisticsData1.previous_average_rate"
         />
       </b-tab>
-      <b-tab :class="{active:tab == 2}" title="참여 통계" value="2" @click="UpdateData()">
-        <div class="row">
-          <div class="col-3"></div>
-          
-          <div class="col-6">
+      <b-tab
+        :class="{ active: tab == 1 }"
+        title="참여 통계"
+        @click="UpdateData(2)"
+      >
+        <div>
+
+          <div class="mx-auto col-12 col-md-6">
             <CategoryDonut
+            :update="update"
               :labels="categoryLabel"
               :dataList="categoryPercent"
             />
           </div>
-          <div class="col-3"></div>
-          <div class="col-3"></div>
-          <div class="col-6">
-            <BodyDonut
-              :labels="bodyLabel"
-              :dataList="bodyPercent"
-            />
+          <div class="mx-auto col-12 col-md-6">
+            <BodyDonut :update="update" :labels="bodyLabel" :dataList="bodyPercent" />
           </div>
         </div>
       </b-tab>
-      <b-tab :class="{active:tab == 3}" title="연령별/성별" value="3" @click="UpdateData()">
+      <b-tab
+        :class="{ active: tab == 2 }"
+        title="연령별/성별"
+        @click="UpdateData(3)"
+      >
         <BodyPrefer
-          :sex="statisticsData.sex"
-          :age="statisticsData.age"
-          :people="statisticsData.people"
+          :sex="statisticsData3.sex"
+          :age="statisticsData3.age"
+          :people="statisticsData3.people"
           :per="bodyPreferPercent"
         />
-          <!-- :age="statisticsData.age" -->
+        <!-- :age="statisticsData.age" -->
         <CategoryPrefer
-          :sex="statisticsData.sex"
-          :age="statisticsData.age"
-          :people="statisticsData.people"
+          :sex="statisticsData3.sex"
+          :age="statisticsData3.age"
+          :people="statisticsData3.people"
           :per="categoryPreferPercent"
         />
       </b-tab>
@@ -59,14 +70,14 @@
 </template>
 
 <script>
-import "../assets/css/StatisticsPage/statistics.css"
-import Calendar from "@/components/StatisticsPage/Calendar.vue"
-import MonthGraph from '../components/StatisticsPage/MonthGraph.vue'
-import AverageRate from '../components/StatisticsPage/AverageRate.vue';
-import CategoryDonut from '../components/StatisticsPage/CategoryDonut.vue';
-import BodyDonut from '../components/StatisticsPage/BodyDonut.vue';
-import BodyPrefer from '../components/StatisticsPage/BodyPrefer.vue';
-import CategoryPrefer from '../components/StatisticsPage/CategoryPrefer.vue';
+import "../assets/css/StatisticsPage/statistics.css";
+import Calendar from "@/components/StatisticsPage/Calendar.vue";
+import MonthGraph from "../components/StatisticsPage/MonthGraph.vue";
+import AverageRate from "../components/StatisticsPage/AverageRate.vue";
+import CategoryDonut from "../components/StatisticsPage/CategoryDonut.vue";
+import BodyDonut from "../components/StatisticsPage/BodyDonut.vue";
+import BodyPrefer from "../components/StatisticsPage/BodyPrefer.vue";
+import CategoryPrefer from "../components/StatisticsPage/CategoryPrefer.vue";
 import { mapGetters } from "vuex";
 import axios from "axios";
 const SERVER_URL = process.env.VUE_APP_SERVER_URL;
@@ -83,84 +94,133 @@ export default {
   },
   data() {
     return {
-      tab: 1,
+      tab: 0,
       idxCategory: 1,
       idxBody: 2,
       sex: "남성",
       age: 20,
       selectedMonth: 0,
-      bodyPercent: [0,0,0,0,0,0,0,0,0],
-      bodyPreferPercent: [0,0,0,0,0,0,0,0,0],
-      categoryPercent: [0,0,0,0,0,0,0,0,0,0],
-      categoryPreferPercent: [0,0,0,0,0,0,0,0,0,0],
-      bodyLabel: ['전신', '상체', '하체', '가슴', '팔', '복부', '엉덩이', '다리', '기타'],
-      categoryLabel: ['요가', '필라테스', '유산소', '댄스', '스트레칭', '근력', '키즈', '복싱', '식단', '기타'],
-      statisticsData: {
-        bodyList: [1,0,1,0,1,0,0,0,0],
-        categoryList: [1,1,1,1,1,1,1,1,1,1],
+      bodyPercent: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      bodyPreferPercent: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      categoryPercent: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      categoryPreferPercent: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      bodyLabel: [
+        "전신",
+        "상체",
+        "하체",
+        "가슴",
+        "팔",
+        "복부",
+        "엉덩이",
+        "다리",
+        "기타",
+      ],
+      categoryLabel: [
+        "요가",
+        "필라테스",
+        "유산소",
+        "댄스",
+        "스트레칭",
+        "근력",
+        "키즈",
+        "복싱",
+        "식단",
+        "기타",
+      ],
+      statisticsData1: {
         monthList: [],
         current_average_rate: 0,
         previous_average_rate: 0,
+      },
+      statisticsData2: {
+        bodyList: [1, 0, 1, 0, 1, 0, 0, 0, 0],
+        categoryList: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      },
+      statisticsData3: {
+        bodyList: [1, 0, 1, 0, 1, 0, 0, 0, 0],
+        categoryList: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         sex: "w",
         age: 20,
         people: 53,
       },
-    }
+      update:false,
+    };
   },
   computed: {
     ...mapGetters(["getUserUid"]),
   },
   watch: {
     selectedMonth() {
-      this.UpdateData()
-    }
+      this.UpdateData(1);
+    },
   },
-  mounted() {
-    this.UpdatePercent()
-    this.UpdateSex()
-  },
+  // mounted() {
+  //   this.UpdatePercent();
+  //   this.UpdateSex();
+  // },
   methods: {
     UpdateMonth(selectedMonth) {
-      this.selectedMonth = selectedMonth
-      this.UpdateData()
+      this.selectedMonth = selectedMonth;
     },
-    UpdateData() {
+    UpdateData(value) {
       axios
-      .get(`${SERVER_URL}/challenge/figures/${this.tab}/${this.selectedMonth}`, {
-        params: {
-          uid: this.getUserUid
-        }
-      })
-      .then((data) => {
-        this.statisticsData = data.data
-        console.log(data)
-      })
-      .catch(() => {
-        alert("챌린지 목록을 불러오지 못했습니다.");
-      });
+        .get(`${SERVER_URL}/challenge/figures/${value}/${this.selectedMonth}`, {
+          params: {
+            uid: this.getUserUid,
+          },
+        })
+        .then((data) => {
+          switch (value) {
+            case 1:
+              this.statisticsData1= data.data;
+              break;
+            case 2:
+              this.statisticsData2= data.data;
+              break;
+              case 3:
+              this.statisticsData3= data.data;
+              break;
+          }
+          this.update = !this.update;
+          if (value == 2) {
+        this.UpdatePercent();
+      }
+          console.log(data);
+        })
+        .catch(() => {
+          alert("챌린지 목록을 불러오지 못했습니다.");
+        });
+
+      
     },
     UpdatePercent() {
-      const reducer = (accumulator, currentValue) => accumulator + currentValue
-      const totalBody = this.statisticsData.bodyList.reduce(reducer)
-      const totalCategory = this.statisticsData.categoryList.reduce(reducer)
-      for (let i = 0; i<10; i++) {
-        this.categoryPercent[i] = (100*this.statisticsData.categoryList[i]/totalCategory).toFixed(0)
-        if (i<9) {
-          this.bodyPercent[i] = (100*this.statisticsData.bodyList[i]/totalBody).toFixed(0)
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      const totalBody = this.statisticsData2.bodyList.reduce(reducer);
+      const totalCategory = this.statisticsData2.categoryList.reduce(reducer);
+      console.log("+++"+totalCategory+"+++"+this.statisticsData2.categoryList);
+      for (let i = 1; i <= 10; i++) {
+        this.categoryPercent[i] = (
+          (100 * this.statisticsData2.categoryList[i]) /
+          totalCategory
+        ).toFixed(0);
+        if (i < 10) {
+          this.bodyPercent[i] = (
+            (100 * this.statisticsData2.bodyList[i]) /
+            totalBody
+          ).toFixed(0);
         }
       }
+      console.log(this.categoryPercent);
     },
     UpdateSex() {
-      if (this.statisticsData.sex === 'm') {
-        this.sex = '남성'
+      if (this.statisticsData3.sex === "m") {
+        this.sex = "남성";
       } else {
-        this.sex = '여성'
+        this.sex = "여성";
       }
-    }
+    },
   },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
