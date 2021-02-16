4d048ef3476063e9310a3722ceda5b7403ce638f
eunsu27 <pesu1027@gmail.com>
2021-02-16 23:44:18 +0900

    [frontend] 기능수정 - 피드목록
    피드목록 하나씩 출력되도록 수정 (row 제거)

--- frontend/src/views/FeedCardList.vue
+++ frontend/src/views/FeedCardList.vue
@@ -1,41 +1,41 @@
 <template>
   <div class="">
     <!-- 피드 -->
-    <div class="row col-12 col-lg-8 feedcardlist-container">
+    <div class="col-12 col-lg-8 feedcardlist-container">
       <feed-card
         v-for="(feed, index) in feedList"
-        class="col-12 challenge-list-feed"
+        class="col-12 col-sm-12 challenge-list-feed"
         :key="`${index}_feed`"
         :feed="feed"
       >
       </feed-card>
       <!-- <FeedCard /> -->
     </div>
-      <infinite-loading
-        ref="InfiniteLoading"
-        @infinite="getData"
-        spinner="waveDots"
-      >
-        <div class="infinite-loading-message" slot="no-more">
-          <b-button @click="scrollUp"
-            >마지막입니다 <b-icon icon="arrow-up-circle"></b-icon
-          ></b-button>
-        </div>
-        <div class="infinite-loading-message" slot="no-results">
-          결과가 없습니다 :(
-        </div>
-        <div class="infinite-loading-message" slot="error">
-          불러오지 못했습니다.
-        </div>
-      </infinite-loading>
+    <infinite-loading
+      ref="InfiniteLoading"
+      @infinite="getData"
+      spinner="waveDots"
+    >
+      <div class="infinite-loading-message" slot="no-more">
+        <b-button @click="scrollUp"
+          >마지막입니다 <b-icon icon="arrow-up-circle"></b-icon
+        ></b-button>
+      </div>
+      <div class="infinite-loading-message" slot="no-results">
+        결과가 없습니다 :(
+      </div>
+      <div class="infinite-loading-message" slot="error">
+        불러오지 못했습니다.
+      </div>
+    </infinite-loading>
   </div>
 </template>
 
 <script>
 import '../assets/css/FeedCard/feedcardlist.css';
 import FeedCard from '../components/FeedCard.vue';
-import InfiniteLoading from "vue-infinite-loading";
-import "@/assets/css/infiniteloading.css";
+import InfiniteLoading from 'vue-infinite-loading';
+import '@/assets/css/infiniteloading.css';
 
 import axios from 'axios';
 import { mapGetters } from 'vuex';
@@ -50,9 +50,9 @@ export default {
   data() {
     return {
       page: 1,
-      feedAllList:null,
+      feedAllList: null,
       feedList: [],
-    }
+    };
   },
   computed: {
     ...mapGetters(['getUserUid']),
@@ -60,24 +60,24 @@ export default {
   methods: {
     async GetFeed() {
       await axios
-      .get(`${SERVER_URL}/feed/all/focus/${this.$route.params.feed_id}`, {
-        params: {
-          uid: this.getUserUid,
-          challenge_id: this.$route.params.challenge_id,
-        },
-      })
-      .then(({ data }) => {
-        this.feedAllList = data;
-        console.log(data);
-      })
-      .catch(() => {
-        alert('에러가 발생했습니다.');
-      });
+        .get(`${SERVER_URL}/feed/all/focus/${this.$route.params.feed_id}`, {
+          params: {
+            uid: this.getUserUid,
+            challenge_id: this.$route.params.challenge_id,
+          },
+        })
+        .then(({ data }) => {
+          this.feedAllList = data;
+          console.log(data);
+        })
+        .catch(() => {
+          alert('에러가 발생했습니다.');
+        });
     },
 
     async getData($state) {
       if (this.feedAllList == null) {
-          await this.GetFeed();
+        await this.GetFeed();
       }
       let getArray = this.feedAllList.slice(
         (this.page - 1) * 10,
@@ -98,7 +98,7 @@ export default {
       window.scrollTo({
         top: 0,
         left: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     },
   },
